9351b4f3fd619ab2248eec6cff5a39ead97f2dc3
liam <83368782+liammyers@users.noreply.github.com>
2021-05-03 15:28:27 -0600

    Add files via upload

---
+++ Homework 22 - Simpsons/script.m
@@ -0,0 +1,9 @@
+function [I] = Simpson(x, y)
+% Numerical evaluation of integral by Simpson's 1/3 Rule
+% Inputs
+%   x = the vector of equally spaced independent variable
+%   y = the vector of function values with respect to x
+% Outputs:
+%   I = the numerical integral calculated
+
+end
